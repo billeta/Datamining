0140f77c9422c041373c1a700328b1a4b35a9ee7
Anaec Billet <billetapro@gmail.com>
2019-09-18 12:50:29 +0200

    code arbres updated

--- codeTParbre.R
+++ codeTParbre.R
@@ -12,6 +12,9 @@ data("ptitanic")
 
 ?ptitanic
 
+#plot the head of the dataset
+head(ptitanic)
+
 #We are going to look at the survived variable
 class(ptitanic$survived)
 summary(ptitanic$survived)
@@ -19,7 +22,7 @@ summary(ptitanic$survived)
 
 #5#
 
-#We will compute our decision tree
+#We will compute our decision tree and plot it
 r <- rpart(survived~., data = ptitanic)
 rpart.plot(r)
 
@@ -31,3 +34,26 @@ summary(ex$survived)
 #Then we can check at proportion of survivant
 sum(ex$survived == "survived") / nrow(ex) # proportion de survivants
 nrow(ex) / nrow(ptitanic) # pourcentage relat. `a tous les exemple
+#Our subset contains 19% of the survivant
+
+#6#
+
+#We create a new passengers
+np <- data.frame("1st", "female", 19, 3, 0) 
+
+#create names
+names(np) <- c("pclass", "sex", "age", "sibsp", "parch")
+
+#Will find the probability that the new passengers died with the decision tree we've created
+predict(r, newdata = np)
+
+#With the biggest class (higher probability)
+predict(r, newdata = np, type = "class")
+#Thus the new passengers had a higher chance to survived on the titanic
+
+#We'll look at the error
+pred.surv <- predict(r, newdata = ptitanic, type = "class")
+
+#We compare our prediction with the reality
+mat.conf <- table(ptitanic$survived, pred.surv)
+mat.conf
