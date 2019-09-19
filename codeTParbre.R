6b219aa317271b21230ce190bb96ccc34f3048cc
Anaec Billet <billetapro@gmail.com>
2019-09-19 15:59:56 +0200

    quest 10 finished

--- codeTParbre.R
+++ codeTParbre.R
@@ -26,8 +26,11 @@ summary(ptitanic$survived)
 r <- rpart(survived~., data = ptitanic)
 rpart.plot(r)
 #In the box we have the label of the response variable
-#Then, the proportion of the other response variable
+#Then, the proportion of the survived people, so a low value with a "died" is good that means that the prediction is good
 #Finally the proportion of the entire data
+
+#For the block in the bottom left:
+#If there is a 27 years old man, the tree say to us that it will die but there is a proportion of 0.17 that it will have survived even if the tree predict DEATH
 
 #Let's take a subset of the male persons in the third class of the titanic
 ex <- subset(ptitanic, sex != "male" & pclass != "3rd") # exemples 
@@ -60,3 +63,159 @@ pred.surv <- predict(r, newdata = ptitanic, type = "class")
 #We compare our prediction with the reality
 mat.conf <- table(ptitanic$survived, pred.surv)
 mat.conf
+
+#Erreur d'apprentissage
+(mat.conf[1,2] + mat.conf[2,1]) / sum(mat.conf)
+#It is low but the model is surely overfitted because we use it on the same set of data
+
+
+#7#
+
+
+#Getting the number of row of the dataset
+n <- nrow(ptitanic)
+
+set.seed(123)
+#Creating the train set ...
+param.app <- 0.7 # proportion of observation for the training set
+
+permut.lignes <- sample(n) # we shuffle the row index
+
+sel <- permut.lignes[1:(param.app * n)] # This the line we select for the train dataset
+
+#Creating the test and train subset of the original data
+train <- ptitanic[sel,]
+test <- ptitanic[-sel,]
+
+#Creating the new model
+r2 <- rpart(survived~., data = train)
+
+#Running prediction on the test dataset
+pred.surv2 <- predict(r2, newdata = test, type = "class") 
+
+#We compare our prediction with the reality
+mat.conf2 <- table(test$survived, pred.surv2)
+mat.conf2
+
+#We can look at a relevant error measure
+(mat.conf2[1,2] + mat.conf2[2,1]) / sum(mat.conf2)
+
+
+#8#
+
+
+?rpart.control
+#minsplit: this is the minimum number of observations that must exist in a node on order for a split to be attempted
+#minsplit == 0 will make the tree go really deeper
+#cp: This is the complexity parameter. This mean how the overall R squared needs to increase for each nodes
+#more cp is low more the tree is deep
+
+#Let's try with those argument
+r3 <- rpart(survived~., data = ptitanic, minsplit = 0, cp = 0) 
+rpart.plot(r3)
+#As we can see the tree is really deep
+
+#the trick is to find the best value of minsplit and cp
+#We can try to findl the best cp for a constant minsplit
+#We'll set it to 20
+minsplit <- 20
+
+#Setting the cp values to try
+mes.cp <- seq(0, 0.1, by = 0.001)
+
+#Setting the corresponding errors for each values
+erreur.test <- numeric(length(mes.cp))
+
+#Will create the erreur function that will compute the error
+
+erreur <- function(matconf){
+  
+  #Computing the error using the mat.conf value
+  (matconf[1,2] + matconf[2,1]) / sum(matconf)
+}
+
+#creating a loop that will for every cp give back the error of the tree (with the confusion matrix)
+for (i in 1:length(mes.cp)) {
+  r4 <- rpart(survived~., data = train, cp = mes.cp[i], minsplit = minsplit) 
+  pred.surv4 <- predict(r4, newdata = test, type = "class") 
+  mat.conf4 <- table(test$survived, pred.surv4) 
+  erreur.test[i] <- erreur(mat.conf4) 
+} 
+
+#Look at the results with a plot
+plot(mes.cp, erreur.test, type = "l")
+abline(v = 0.012, col = "red")
+#cp equal to 0.012 seems good, let's run the model with it
+
+#We create a new model
+r5 <- rpart(survived~., data = ptitanic, cp = 0.012, minsplit = 20) 
+rpart.plot(r5)
+#The fact that is looks like the first one is just a coincidence
+
+
+#9#
+
+
+#Now we look at the accuracy of our prediction on a new dataset. Splitting into a train set and a test set
+#Allows us to not have an overfitted model and to look if our model performs well in other new dataset
+#Another way to go beyond train and test is to try a cross validation
+
+
+#10#
+
+
+#Dependencies
+library(mlbench)
+
+#Loading the dataset
+data("Zoo")
+
+#We are going to look at the new dataset
+head(Zoo)
+summary(Zoo)
+?Zoo
+
+#our response variable will be the type variable, we'll try to predic the type of the animal
+#First let's create the train and test dataset
+set.seed(42)
+
+
+
+sel <- permut.lignes[1:(param.app * n)] # This the line we select for the train dataset
+
+#Creating the test and train subset of the original data
+train <- Zoo[sel,]
+test <- Zoo[-sel,]
+
+#We are going to compute a function that select the best value of cp and minsplit
+best.minsplit.cp <- function(mes.cp = seq(0, 0.1, by = 0.001), mes.minsplit = seq(0, 50, by = 1), dataset){
+  
+  #Initializing the vectors errors
+  
+  #Creating test and train
+  param.app <- 0.7 # proportion of observation for the training set
+  permut.lignes <- sample(n) # we shuffle the row index
+  train <- Zoo[dataset,]
+  test <- Zoo[-dataset,]
+  
+  #starting the testing
+  #Looping for each value of minsplit
+  
+}
+
+ 
+  
+
+
+
+  for (i in 1:length(mes.cp)) {
+    r4 <- rpart(survived~., data = train, cp = mes.cp[i], minsplit = minsplit) 
+    pred.surv4 <- predict(r4, newdata = test, type = "class") 
+    mat.conf4 <- table(test$survived, pred.surv4) 
+    erreur.test[i] <- erreur(mat.conf4) 
+  } 
+
+
+
+
+
